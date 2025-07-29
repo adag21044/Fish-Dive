4cc62315b2528d96183a4fdf35c91af131a3d866
adag21044 <adag21044@gmail.com>
2025-07-29 11:27:01 +0300

    Add speed property and auto-move to BubbleMover
    Introduced a serialized speed field to BubbleMover and updated the script to automatically move the bubble upward each frame. Updated the Baloon prefab to include the new BubbleMover component with a default speed value.

--- Assets/Scripts/Bubble/Bubble Mover/BubbleMover.cs
+++ Assets/Scripts/Bubble/Bubble Mover/BubbleMover.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class BubbleMover : MonoBehaviour
 {
+
+    [SerializeField] private float speed = 2.5f;
+    private void Update()
+    {
+        MoveBubble(Vector3.up, speed);
+    }
+
     // TODO: Create safe destination distance between bubbles
-    public void MoveBubble(Vector3 direction, float speed)
+
+    private void MoveBubble(Vector3 direction, float speed)
     {
         // Move the bubble in the specified direction at the given speed
         transform.position += direction * speed * Time.deltaTime;
